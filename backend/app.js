a9d25671a64c2d524fdbcc2c57af9a27d51033fd
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-22 20:02:54 +0530

    cc

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config({ path: "backend/config/config.env" });
 
+// Import all routes
+import productRoutes from "./routes/products.js";
+
+app.use("/api", productRoutes);
+
 app.listen(process.env.PORT, () => {
-  console.log("up on port", process.env.PORT);
+  console.log(
+    `Up on port ${process.env.PORT} and ${process.env.NODE_ENV} mode`
+  );
 });
