249e72e6139157f9ab6c69c9b734516c9f1c5d48
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-22 21:34:05 +0530

    added seeder

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,17 +2,18 @@ import Product from "../models/product.js";
 
 // Get all products   =>  /api/products
 export const getProducts = async (req, res) => {
+  const products = await Product.find();
+
   res.status(200).json({
-    message: "All Products",
+    products,
   });
 };
 
 // Create new Product   =>  /api/admin/products
 export const newProduct = async (req, res) => {
-    const product = await Product.create(req.body);
-  
-    res.status(200).json({
-      product,
-    });
-  };
-  
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    product,
+  });
+};
