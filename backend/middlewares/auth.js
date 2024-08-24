64b9af04b83b99290332a9233b55a0b7fbc73fdd
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-24 10:37:42 +0530

    auth user roles

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,4 +16,20 @@ export const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   
     next();
   });
+
+  // Authorize user roles
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role (${req.user.role}) is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
   
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,10 +6,10 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/productControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser, getProducts);
+router.route("/products").get(isAuthenticatedUser, authorizeRoles("admin"), getProducts);
 router.route("/admin/products").post(newProduct);
 router.route("/products/:id").get(getProductDetails);
 router.route("/products/:id").put(updateProduct);
