ebceeb25a7c70236d5c4a5f175fa7af413e77939
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-25 11:07:06 +0530

    get user profile (current)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,8 +4,10 @@ import {
   loginUser,
   logout,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  getUserProfile,
 } from "../controllers/authControllers.js";
+import { isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -14,5 +16,7 @@ router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
+// user routes
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 export default router;
