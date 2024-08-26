7ae409c2237e9341e83a48216d11e7cf45d23683
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-26 20:20:29 +0530

    redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Home from "./components/Home";
-import MetaData from "./components/layout/MetaData";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import { productApi } from "./api/productsApi";
+
+export const store = configureStore({
+  reducer: {},
+});
