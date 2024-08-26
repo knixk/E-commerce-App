0308bac6a3069be286c9cbc0319d3ed868b5aa2f
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-08-26 14:18:22 +0530

    create review, delete review

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -5,14 +5,31 @@ import {
   getProductDetails,
   updateProduct,
   deleteProduct,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
 } from "../controllers/productControllers.js";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-router.route("/admin/products").post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+router
+  .route("/admin/products")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
 router.route("/products/:id").get(getProductDetails);
-router.route("/admin/products/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
-router.route("/admin/products/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+router
+  .route("/admin/products/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router
+  .route("/reviews")
+  .put(isAuthenticatedUser, createProductReview)
+  .get(isAuthenticatedUser, getProductReviews);
+
+router
+  .route("/admin/reviews")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
 
 export default router;
