feb5725eaa35cbdc19607b9aceb2f1a09598112a
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-02 12:27:35 +0530

    fixes

--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productApi = createApi({
   reducerPath: "productApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/v1" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: (params) => "/products",
