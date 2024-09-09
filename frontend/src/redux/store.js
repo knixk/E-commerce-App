d3f79f436e766332432083648efce31ad527d7a7
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-09 18:36:39 +0530

    changes

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productApi } from "./api/productsApi";
 import { authApi } from "./api/authApi";
+import { userApi } from "./api/userApi";
 
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([productApi.middleware, authApi.middleware]),
+    getDefaultMiddleware().concat([
+      productApi.middleware,
+      authApi.middleware,
+      userApi.middleware,
+    ]),
 });
